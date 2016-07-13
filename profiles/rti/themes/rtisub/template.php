9644c4ad701abe70a91c05e0bcd0db958e8cb442
Rick Hastie <rhastie@austin.utexas.edu>
2016-07-13 15:43:47 -0500

    edit template.php to fix conditional to set variables for only certain views

--- profiles/rti/themes/rtisub/template.php
+++ profiles/rti/themes/rtisub/template.php
@@ -84,7 +84,8 @@ function rtisub_preprocess_node(&$variables) {
 }
 
 function rtisub_preprocess_views_view_fields(&$variables) {
-  if ($variables['view']->name == "rti_indexed_search" || "rti_feaured_items" || "rti_parents") {
+
+if ($variables['view']->name == "rti_indexed_search" || $variables['view']->name == "rti_feaured_items" || $variables['view']->name == "rti_parents") {
     $term_id = $variables['row']->_entity_properties['field_resource_type'];
     $term = taxonomy_term_load($term_id);
     $term_name = $term->name;
