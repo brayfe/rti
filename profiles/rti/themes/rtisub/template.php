50ba5925d3c79b864a9b7b62924b500b41f2d765
Tyler Fahey <tfahey@austin.utexas.edu>
2017-01-30 11:42:37 -0600

    Tweak to “New Search” that performs new query w/o facets in play

--- profiles/rti/themes/rtisub/template.php
+++ profiles/rti/themes/rtisub/template.php
@@ -31,11 +31,12 @@ function rtisub_preprocess_maintenance_page(&$variables, $hook) {
 function rtisub_views_pre_render(&$view) {
   if ($view->name == 'rti_indexed_search' && $view->current_display == 'page') {
     $block = module_invoke('views', 'block_view', '-exp-rti_indexed_search-page');
-    $block['content']['#markup'] = str_replace('Search', 'Refine Search', $block['content']['#markup']);
+    // $block['content']['#markup'] = str_replace('Search', 'Refine Search', $block['content']['#markup']);
     $view->attachment_before = '<div class="main-region-search-form">' .
-    $block['content']['#markup'] . '<span class="reset-btn"><a href="/rti-search">New Search</a></span></div>';
+    $block['content']['#markup'] . '</div>';
   }
 }
+
 
 
 /**
@@ -197,6 +198,30 @@ function rtisub_preprocess_views_view_fields(&$variables) {
       ));
     }
   }
+}
+
+/**
+ * Implements hook_form_alter().
+ */
+function rtisub_form_alter(&$form, &$form_state, $form_id) {
+  switch ($form['#id']) {
+    case 'views-exposed-form-rti-indexed-search-page':
+      $form['rti-search-new-search'] = array(
+        '#type' => 'submit',
+        '#value' => 'New Search!!!',
+        '#submit' => array('rtisub_search_new_search'),
+      );
+      break;
+  }
+}
+
+/**
+ * Custom New Search callback for RTI Search page.
+ */
+function rtisub_search_new_search($form, &$form_state) {
+  $search_text = $form_state['values']['search_text'];
+  $options = array('query' => array('search_text' => $search_text));
+  drupal_goto("/rti-search", $options);
 }
 
 /**
